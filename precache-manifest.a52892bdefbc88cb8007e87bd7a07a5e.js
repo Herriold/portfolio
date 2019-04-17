25266186fcc67284cbd2c9c24a0ace7698b33a16
herrio <ratsimbazafyherrio@gmail.com>
2019-04-17 14:59:01 +0200

    Updates

---
+++ precache-manifest.a52892bdefbc88cb8007e87bd7a07a5e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "06a8af000338d5c74cae",
+    "url": "/portfolio/static/js/main.2b6b3bdb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ffe4e0c50bd791e38980",
+    "url": "/portfolio/static/js/2.d417e94f.chunk.js"
+  },
+  {
+    "revision": "8e52e941422a387b43ff2d743121c75c",
+    "url": "/portfolio/index.html"
+  }
+];
