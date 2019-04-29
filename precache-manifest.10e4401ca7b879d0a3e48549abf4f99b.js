3544a0ea96c09c3173f8dc32f589761f7a3df80c
herrio <ratsimbazafyherrio@gmail.com>
2019-04-29 14:36:09 +0200

    Updates

---
+++ precache-manifest.10e4401ca7b879d0a3e48549abf4f99b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f032080da4765f8e605",
+    "url": "/portfolio/static/css/main.7a2ae249.chunk.css"
+  },
+  {
+    "revision": "2f032080da4765f8e605",
+    "url": "/portfolio/static/js/main.3ee78439.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bb07a631393b29ae4c35",
+    "url": "/portfolio/static/js/2.a90bc6ec.chunk.js"
+  },
+  {
+    "revision": "9745115e3b1358902b416b3c18d61a87",
+    "url": "/portfolio/static/media/call-to-action-background.9745115e.jpg"
+  },
+  {
+    "revision": "6bd729cbbabd88ade3ab2c7ca3e93c8d",
+    "url": "/portfolio/static/media/phone-in-hand.6bd729cb.jpg"
+  },
+  {
+    "revision": "91d8c9d24541cb3b10befeb8380fbc13",
+    "url": "/portfolio/static/media/headers.91d8c9d2.jpg"
+  },
+  {
+    "revision": "a7447c9ee16414e9a71cce8a0c071743",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.24b4424d7aa6e919f486cc0756af684f.js"
+  "/portfolio/precache-manifest.10e4401ca7b879d0a3e48549abf4f99b.js"
 );
 
 workbox.clientsClaim();
